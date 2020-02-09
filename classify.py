318e3bf52c9f9ff7f79d28a255d997dde3c0bc9a
Ryan Lee <ryanlee7132@gmail.com>
2020-02-09 14:21:17 -0500

    Update classify.py

--- classify.py
+++ classify.py
@@ -7,22 +7,21 @@ from google.cloud import language_v1
 from google.cloud.language_v1 import enums
 
 #Text must be at least 20 words long for an accurate classification.
-#text_content = 'the computer is one of the most wonderful inventions of modern technology. It is fairly a recent invention. It has now become a part and parcel of day-to-day life. The computer was invented by an English mathematician called Charles Babbage. Computers are two basic types: analog and digital. Analog: computers deal with physical qualities and digital computers deal with numbers. Computers have five major components; input, store, control, processing, and output.'
+text_content = 'the computer is one of the most wonderful inventions of modern technology. It is fairly a recent invention. It has now become a part and parcel of day-to-day life. The computer was invented by an English mathematician called Charles Babbage. Computers are two basic types: analog and digital. Analog: computers deal with physical qualities and digital computers deal with numbers. Computers have five major components; input, store, control, processing, and output.'
 
-def classify_image (str)
-        client = language_v1.LanguageServiceClient()
-        type_ = enums.Document.Type.PLAIN_TEXT
+client = language_v1.LanguageServiceClient()
+type_ = enums.Document.Type.PLAIN_TEXT
         
-        language = "en"
-        document = {"content": text_content, "type": type_, "language": language}
+language = "en"
+document = {"content": text_content, "type": type_, "language": language}
 
-        response = client.classify_text(document)
+response = client.classify_text(document)
 
-        print(text_content)
+print(text_content)
 
-        for category in response.categories:
-               print(u"Category name: {}".format(category.name))
-               print(u"Confidence: {}".format(category.confidence))
+for category in response.categories:
+        print(u"Category name: {}".format(category.name))
+        print(u"Confidence: {}".format(category.confidence))
 
 def request_text_classification_api(text_content):
         api_key = read_google_api_key()
